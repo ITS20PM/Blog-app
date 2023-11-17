572baec730668cfb50ef5fa4a53435097bbd3919
ITS20PM <patrick21meng@gmail.com>
2023-11-17 13:01:20 -0800

    PostDetailView template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -22,5 +23,15 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+class PostDetailView(DetailView):
+    model = Post
+
+
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
