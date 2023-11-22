aca7e9b1ecf798f58c1f07bedcf9f006ba7ddceb
ITS20PM <patrick21meng@gmail.com>
2023-11-21 17:01:42 -0800

    version 1

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!s046il&9^(cpm5f_(4c3-$w5f^pqx41(nzp*zvv40&q(+)^hc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
